d859d24b92e5669240b3c79b81004776db1c291c
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-07-23 15:40:55 +0530

    code optimization super admin

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -49,7 +49,6 @@
 
     @include('backend.layouts.include.footer')
     @include('backend.layouts.include.js')
-    @include('backend.layouts.include.myjs')
     @yield('extrascript')
 
     <!-- Page JS -->
