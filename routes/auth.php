cd3a2c1d40fc163982d1490349ffecde9eac59c6
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-08-26 15:42:31 +0530

    add tenent route in php

--- routes/auth.php
+++ routes/auth.php
@@ -1,61 +1,41 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\App\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\App\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\App\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\App\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\App\Auth\NewPasswordController;
+use App\Http\Controllers\App\Auth\PasswordResetLinkController;
+use App\Http\Controllers\App\Auth\RegisteredUserController;
+use App\Http\Controllers\App\Auth\VerifyEmailController;
+use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-//     Route::get('register', [RegisteredUserController::class, 'create'])
-//                 ->name('register');
+    Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
 
-//    Route::post('register', [RegisteredUserController::class, 'store']);
-// dd("hello11");
-Route::get('/', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-    //             ->name('password.request');
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
 
-    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-    //             ->name('password.email');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
+    Route::get('new-password', [PasswordResetLinkController::class, 'newPassword'])->name('new.password');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
+    Route::post('store-new-password', [PasswordResetLinkController::class, 'storeNewPassword'])->name('store.new.password');
 
-    Route::get('new-password', [PasswordResetLinkController::class, 'newPassword'])
-        ->name('new.password');
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
 
-    Route::post('store-new-password', [PasswordResetLinkController::class, 'storeNewPassword'])
-        ->name('store.new.password');
-
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.store');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.update');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
+    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
                 ->middleware(['signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
@@ -68,8 +48,8 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
     Route::any('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+    Route::resource('tenants',TenantController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,159 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TenantController;
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\{
+    InitializeTenancyByDomain,
+    PreventAccessFromCentralDomains
+};
+use App\Http\Controllers\App\{
+    ProfileController,
+    UserController
+};
+
 use App\Http\Controllers\backends\{
-    DashboardController,
     HomeController,
     AuthController
 };
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\App\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\App\UserController;
-use App\Http\Controllers\Tenant\ServiceController;
-use App\Models\Order;
-use App\Models\Tenant;
-use App\Models\User;
+use App\Http\Controllers\Tenant\{
+    DashboardController,
+    ClientController,
+    OrderController,
+    CategoryController,
+    ServiceController,
+    PaymentController,
+    InvoiceController,
+    ItemTypeController
+};
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+| Feel free to customize them however you want. Good luck!
 |
 */
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+    ])->group(function () {
 
-Route::middleware('auth')->group(function () {
-    // Route for the dashboard
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])
-        ->name('dashboard'); // Name for route reference
-
-
-    // Profile management routes
-    Route::controller(HomeController::class)->group(function () {
-        Route::get('/myProfile', 'myprofile')->name('myProfile');
-        Route::get('/edit/profile/{id}', 'editprofile')->name('edit.profile');
-        Route::post('/profile/update/{id}', 'updateprofilepost')->name('profile.update');
+        Route::get('admin/Categorylists', function () {
+            return view('admin.CategoriesLists');
+        });
+    // Welcome page
+    Route::get('/', function () {
+        return view('app.welcome');
     });
 
-    // Password management routes
-    Route::controller(AuthController::class)->group(function () {
-        Route::get('/change/password', 'changePassword')->name('change.password');
-        Route::post('/change/password/post', 'changePasswordPost')->name('change.password.post');
+    // Dashboard page with client and order counts
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware(['auth'])
+        ->name('dashboard');
+
+    // Authenticated routes
+    Route::middleware('auth')->group(function () {
+
+        // Profile management routes
+        Route::controller(HomeController::class)->group(function () {
+            Route::get('/myProfile', 'myprofile')->name('myProfile');
+            Route::get('/edit/profile/{id}', 'editprofile')->name('edit.profile');
+            Route::post('/profile/update/{id}', 'updateprofilepost');
+        });
+
+        // Password management routes
+        Route::controller(AuthController::class)->group(function () {
+            Route::get('/change/password', 'changePassword')->name('change.password');
+            Route::post('/change/password/post', 'changePasswordPost')->name('change.password.post');
+        });
+
+        // User management routes
+        Route::resource('users', UserController::class);
+
+        // Client management routes
+        Route::controller(ClientController::class)->prefix('admin')->group(function () {
+            Route::get('/client', 'index')->name('clientpage');
+            Route::post('/add-client', 'addClient')->name('add.client');
+            Route::post('/edit-client/{id}', 'editClient');
+            Route::get('/delete-client/{id}', 'deleteClient');
+        });
+
+        // Order management routes
+        Route::controller(OrderController::class)->prefix('admin')->group(function () {
+            Route::get('/order', 'index')->name('addOrder');
+            Route::post('/add-order', 'addOrder')->name('add.order');
+            Route::post('/get-service', 'getServiceData');
+            Route::post('/get-allservice', 'getAllServiceData');
+            Route::get('edit-order/{id}', 'editOrder')->name('order.edit');
+            Route::put('update-order/{id}', 'updateOrder')->name('order.update');
+            Route::get('/view-order', 'viewOrder')->name('viewOrder');
+            Route::get('/show-order/{orderId}', 'OrderDetail')->name('OrderDetail');
+            Route::get('/delete-order/{id}', 'deleteOrder');
+            Route::get('/receipt/{orderId}', 'PrintReceipt')->name('receipt');
+            Route::get('/invoice/{orderId}', 'PrintInvoice')->name('invoicepdf');
+            Route::get('/tagslist/{orderId}', 'tagList')->name('tagslist');
+            Route::get('/print-taglist/{orderId}', 'printTaglist')->name('download-tagslist');
+            Route::match(['get', 'post'], '/send-wh-message/{orderId}', 'sendWhMessage')->name('orders.store');
+            Route::get('/fetch-client-name', 'fetchClientName');
+            Route::get('/download-receipt/{orderId}', 'downloadReceipt')->name('download-receipt');
+            Route::get('/download-invoice/{orderId}', 'downloadInvoice')->name('download-invoice');
+            Route::get('/get-services', 'getServices')->name('getServices');
+            Route::get('/get-price', 'getPrice')->name('getprice');
+            Route::get('/receipt-print/{orderId}', 'RecieptPrint')->name('receipt-print');
+            Route::get('/invoice-print/{orderId}', 'InvoicePrint')->name('invoice-print');
+        });
+
+        // Category management routes
+        Route::controller(CategoryController::class)->prefix('admin')->group(function () {
+            Route::get('/categorylist', 'index')->name('categorylist');
+            Route::get('/category', 'addcategory')->name('category');
+            Route::post('/category-add', 'storeCategory')->name('add.category.details');
+            Route::get('/fetch-data-clothes', 'fetchClothesData');
+            Route::get('/fetch-data-upholstrey', 'fetchUpholsteryData');
+            Route::get('/fetch-data-footbags', 'fetchFootBagData');
+            Route::get('/fetch-data-other', 'fetchOtherData');
+            Route::get('/fetch-data-laundry', 'fetchLaundryData');
+            Route::post('/delete-clothes/{id}', 'deleteClothes');
+            Route::post('/categorylist', 'editItems');
+        });
+
+        // Service management routes
+        Route::controller(ServiceController::class)->prefix('admin')->group(function () {
+            Route::get('/service', 'index')->name('service');
+            Route::post('/add-service', 'addService')->name('add.service');
+            Route::post('/edit-services/{id}', 'updateService');
+            Route::get('/delete-services/{id}', 'deleteService');
+        });
+
+
+        // Item Type management routes
+        Route::controller(ItemTypeController::class)->prefix('admin')->group(function () {
+            Route::get('/itemtype', 'index')->name('itemtype');
+            Route::post('/add-itemtype', 'addType')->name('add.itemtype');
+            Route::post('/edit-itemtype/{id}', 'updateItemType');
+            Route::get('/delete-itemtype/{id}', 'deleteItemType');
+        });
+
+        // Payment management routes
+        Route::controller(PaymentController::class)->prefix('admin')->group(function () {
+            Route::get('/payment', 'index')->name('payment');
+            Route::post('/settle-and-deliver-order/{orderId}', 'settleAndDeliverOrder');
+        });
+
+        // Invoice management routes
+        Route::controller(InvoiceController::class)->prefix('admin')->group(function () {
+            Route::get('/invoice', 'index')->name('invoice');
+            Route::get('/indexfilter', 'indexfilter')->name('indexfilter');
+            Route::get('/orders/export', 'export')->name('orders.export');
+            Route::get('/orders/analitices', 'analitices')->name('orders.analitices');
+        });
     });
 
-    // Resource routes for managing tenants
-    Route::resource('tenants', TenantController::class);
-
-    // Custom route for deleting a tenant, typically used for AJAX requests
-    Route::post('/admin/delete-tenant/{id}', [TenantController::class, 'deleteTenant'])
-        ->name('tenant.delete');
+    // Load tenant authentication routes
+    require __DIR__ . '/tenant-auth.php';
 });
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-require __DIR__ . '/auth.php';
